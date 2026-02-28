37eab967e2a06bab3faa94c36605850fbccba2eb
khushi-gunnu17 <khushiastrogeek@gmail.com>
2026-02-28 15:20:24 +0530

    tasks

--- Playwright/tests/Day_11_task2.spec.ts
+++ Playwright/tests/Day_11_task2.spec.ts
@@ -4,7 +4,8 @@ test("task2",async({page})=>{
     await page.goto("https://www.flipkart.com/");
     await page.locator('//div/input').first().fill("Phones");
     await page.keyboard.press("Enter");
-    await page.locator('//label').first().click();
-    console.log(await page.locator('//div[@class="QiMO5r"]/div').nth(3).textContent());
-    await page.screenshot({path:"screenshot/task2.png"});
+    await page.locator("(//div[@class='ybaCDx'])[1]").click();
+   const price =await page.locator('(//div[@class="hZ3P6w DeU9vF"])[3]').textContent();
+   console.log(price);
+   await page.screenshot({path:"screenshot/question_2"+Date.now()+".png"});
 });
